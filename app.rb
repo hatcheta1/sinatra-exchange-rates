985e8b17b2d50fa33f4a31715ff683809e2fcd1a
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-10 19:02:32 +0000

    updates homepage view template to display currencies

--- app.rb
+++ app.rb
@@ -3,17 +3,15 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
-exchange_rate_key = ENV.fetch("EXCHANGE_RATE_KEY")
-puts exchange_rate_key
-
 get("/") do
-  exchange_rate_url = "https://api.exchangerate.host/list?access_key=#{exchange_rate_key}"
+  exchange_rate_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
 
-  resp = HTTP.get(exchange_rate_url)
-  raw_response = resp.to_s
-  parsed_response = JSON.parse(raw_response)
+  @raw_response = HTTP.get(exchange_rate_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
 
-  @currencies = parsed_response.fetch("currencies")
+  @currencies = parsed_response.fetch("currencies").keys
+
   erb(:homepage)
 end
 
