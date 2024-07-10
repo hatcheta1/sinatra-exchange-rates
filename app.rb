391939e70afc2420d0f2aa10f0ef19ecf0215882
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-10 18:57:04 +0000

    working on accessing currencies from exchangerate.host

--- app.rb
+++ app.rb
@@ -3,9 +3,17 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
-exchange_rate_key = EXCHANGE_RATE_KEY
+exchange_rate_key = ENV.fetch("EXCHANGE_RATE_KEY")
+puts exchange_rate_key
 
 get("/") do
+  exchange_rate_url = "https://api.exchangerate.host/list?access_key=#{exchange_rate_key}"
+
+  resp = HTTP.get(exchange_rate_url)
+  raw_response = resp.to_s
+  parsed_response = JSON.parse(raw_response)
+
+  @currencies = parsed_response.fetch("currencies")
   erb(:homepage)
 end
 
