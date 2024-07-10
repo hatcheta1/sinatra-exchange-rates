eebe8b14d74213551aed24c001f25af907f76b3f
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-10 18:42:55 +0000

    added routes and view templates

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
+exchange_rate_key = EXCHANGE_RATE_KEY
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/:currency_to_convert") do
+  erb(:to_convert)
+end
+
+get("/:currency_to_convert/:conversion_currency") do
+  erb(:conversion)
 end
